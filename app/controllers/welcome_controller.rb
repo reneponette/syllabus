61d40af9d50e7bc59ebf2189c7c293fdbb1d59c8
rene <rene@kakao.com>
2014-03-04 07:04:36 -0800

    모바일로 들어오면 웰컴페이지 건너뛰도록 수정

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,18 @@
 class WelcomeController < ApplicationController
+  
   def index
+    if mobile_device?    
+      redirect_to :products
+    end    
   end
+  
+  
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+    
 end
